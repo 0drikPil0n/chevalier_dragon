ee2ed84db084774d859fc9a6f64bdf931acf8a93
Tryx <tristanrobichaud1@gmail.com>
2025-02-28 09:48:07 -0500

    Constructeur Combat
    Constructeur de combat fini

---
+++ Combat.py
@@ -0,0 +1,7 @@
+from Chevalier import Chevalier
+from Dragon import Dragon
+
+class Combat:
+    def __init__(self, dragon: Dragon, chevalier: Chevalier):
+        self.dragon = dragon
+        self.chevalier = chevalier
