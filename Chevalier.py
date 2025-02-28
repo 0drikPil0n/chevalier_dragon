523fa267170ef9e3476d4eeca722dc34fd7a0dc8
Samuel <6246923@cegepoutaouais.qc.ca>
2025-02-28 09:59:25 -0500

    combat commencer

--- Chevalier.py
+++ Chevalier.py
@@ -5,3 +5,7 @@ class Chevalier:
         self.pts_vie = pts_vie
         self.pts_attaque = pts_attaque
         self.protection = protection
+
+
+    def __str__(self):
+        return f"le chevalier {self.nom} prend {self.armes}, se protège avec {self.protection} et a {self.pts_vie} de points vie {self.pts_attaque} de points d'attaques"
--- Combat.py
+++ Combat.py
@@ -1,7 +1,19 @@
 from Chevalier import Chevalier
 from Dragon import Dragon
 
+
 class Combat:
     def __init__(self, dragon: Dragon, chevalier: Chevalier):
         self.dragon = dragon
         self.chevalier = chevalier
+
+
+    def vainqueur(self):
+        if self.chevalier.pts_attaque > self.dragon.point_vie:
+            return self.chevalier
+        else:
+            return self.dragon
+
+
+    def afficher_vainqueur(self):
+        return f"Le vainqueur : {self.vainqueur()}"
